2d5d3eb94bd7982edc232e72d06591fcf9a6ee17
Shahzad Farukh <shahzadfarukh100@gmail.com>
2019-06-10 13:20:14 +0500

    bug fix: decode error

--- appengine_config.py
+++ appengine_config.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # appengine_config.py
 from google.appengine.ext import vendor
 
@@ -19,12 +20,14 @@ keys = DataList.query().fetch(keys_only=True)
 ndb.delete_multi(keys)
 time.sleep(5)
 
-
-with open('csv/data_list.csv') as csvfile:
-	csvread = csv.reader(csvfile, delimiter=',')
-	for row in csvread:
-		data_list = DataList(type_name=row[0], description= row[1], link=row[2])
-		data_list.put()
+try:
+	with open('csv/data_list.csv') as csvfile:
+		csvread = csv.reader(csvfile, delimiter=',')
+		for row in csvread:
+			data_list = DataList(type_name=row[0], description= row[1].decode('iso-8859-1') , link=row[2])
+			data_list.put()
+except AssertionError as error:
+	print"error"
 
 ###################### End DataList  ########################
 
